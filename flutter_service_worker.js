3f220a8869d2860b1803dbc508803393f1622a45
Timo Hannemann <timo.hannemann@hani-web.de>
2025-09-11 19:21:49 +0200

    added writer reader separation

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "4f9b16fcd02aafcf737dbe6d4a4477cd",
+"flutter_bootstrap.js": "6971c7d6e46b785ebf52354e08759322",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "8d15509cdf7547aec3ac34c0863d3a0d",
 "/": "8d15509cdf7547aec3ac34c0863d3a0d",
-"main.dart.js": "d38675fc9cc965546249b972d9baa9a2",
+"main.dart.js": "fad4c7918bbf3bc606378527f752200c",
 "manifest.json": "efaa0307031036c038e5df2d91a8a03e",
 "version.json": "51b670a22f851c46b3bdcb61922b2624"};
 // The application shell files that are downloaded before a service worker can
